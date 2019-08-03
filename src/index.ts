5ddc12f404f6ccf854625cfc8cb3b64ef5f14e61
Mel DeJesus <meldejesus22@gmail.com>
2019-08-03 06:58:08 -0400

    first

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+console.log("hello")
+
+var x:string = "hello";
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var webpack = require('webpack');
+// entry: the file to which all other things are connected
+//output point: 
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'false',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'dist.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        // Include ts, tsx, js, and jsx files.
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        
+      },
+    ],
+  },
+  plugins: [],
+};
